53e82f3d361c184c86cd2c1b386d6c8a115a1de0
Mrunali <mrunalijibhakate04@gmail.com>
2026-01-13 22:10:48 +0530

    started!

---
+++ 01_day/1_numpy_basics.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5])
+print("Array:", arr)
+print("Sum:", arr.sum())
+print("Mean:", arr.mean())
+print("Max:", arr.max())
+
+a = np.array([1, 2, 3])
+b = np.array([10, 20, 30])
+
+print("Addition:", a + b)
+print("Multiplication:", a * b)
+
+matrix = np.array([[1, 2, 3], [4, 5, 6]])
+print("Shape:", matrix.shape)
+print("First row:", matrix[0])
+print("Second column:", matrix[:, 1])
